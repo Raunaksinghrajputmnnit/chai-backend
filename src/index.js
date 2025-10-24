5694724ad42f99750f921a765bd4115f1117db3b
raunak singh <raunaksingh@raunaks-MacBook-Air.local>
2025-10-24 17:05:03 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,32 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is listening on port :${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 import express from "express";
 const app=express()
