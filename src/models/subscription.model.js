addc8b2a6a22f30c6369f8310ba69f14235d0137
raunak singh <raunaksingh@raunaks-MacBook-Air.local>
2025-10-29 17:42:51 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,13 @@
+
+import mongoose ,{Schema} from "mongoose";
+const subscriptionSchema=new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,// one who is subscriing
+        ref:"User"
+
+    },
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
